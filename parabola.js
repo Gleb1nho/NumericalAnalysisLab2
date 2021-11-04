227c99a0837859b3d07291f1ce7c48da0b306a10
Gleb1nho <55406035+Gleb1nho@users.noreply.github.com>
2021-11-04 18:09:30 +0500

    Update parabola method, change criterion to stop

--- parabola.js
+++ parabola.js
@@ -4,6 +4,7 @@ const f = (x) => { return Math.sin(x) + 0.1 - 2*x*x }
 const derivative = (x) => { return Math.cos(x) - 4*x }
 const derivative2 = (x) => { return -(Math.sin(x))-4 }
 const M = 4.3
+const m = 1.57
 const x0 = 0.6
 const calculateNextX = (xn) => { 
   return xn+(derivative(xn)+Math.sqrt(derivative(xn)*derivative(xn)+2*M*f(xn)))/M
@@ -13,7 +14,7 @@ let xn = calculateNextX(x0)
 let previousValue = x0
 let n = 1
 
-while (Math.abs(xn-previousValue) > epsilon) {
+while ((M/m)*(xn-previousValue)*(xn-previousValue) >= epsilon) {
   let temp = xn
   xn = calculateNextX(xn)
   previousValue = temp
